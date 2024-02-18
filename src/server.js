6bf6309bbc2c1a17d165098fc27c47ee8d759e6d
Abhishek <abhishek.pal0910@gmail.com>
2024-02-18 22:11:46 +0530

    1- commonJs to ES6

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const { createServer } = require('http');
+import "dotenv/config";
+import { createServer } from "http";
 
-const app = require('./app');
+import app from "./app.js";
 
 const PORT = process.env.PORT || 8000;
 
 const server = createServer(app);
 
 function startServer() {
-	server.listen(PORT, () => console.log(`Server running on port: ${PORT}...`));
+  server.listen(PORT, () => console.log(`Server running on port: ${PORT}...`));
 }
 
 startServer();

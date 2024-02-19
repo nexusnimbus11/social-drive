f99f56d9b6820efcfdb0dc59dacb790257f08d0c
Prateek Singh <prateeksingh298@gmail.com>
2024-02-19 23:01:45 +0530

    add prettier config and implemented in all files

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.all("*", (req, res) => {
-  return res.sendStatus(404);
+app.all('*', (req, res) => {
+    return res.sendStatus(404);
 });
 
 export default app;

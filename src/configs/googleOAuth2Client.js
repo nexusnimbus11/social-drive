f5feb23e148233bbfd5f70e243987e7578d10cda
Prateek Singh <prateeksingh298@gmail.com>
2024-03-10 23:24:36 +0530

    feat: SD-16 configure google oauth2 + temp client page

---
+++ src/configs/googleOAuth2Client.js
@@ -0,0 +1,28 @@
+import { OAuth2Client } from 'google-auth-library';
+
+/**
+ * Creates an oAuth2 client to authorize the API call
+ */
+export function getOAuth2Client() {
+    const oAuth2Client = new OAuth2Client(
+        process.env.GOOGLE_CLIENT_ID,
+        process.env.GOOGLE_CLIENT_SECRET,
+        process.env.GOOGLE_OAUTH_REDIRECT_URI
+    );
+
+    return oAuth2Client;
+}
+
+/**
+ * Generate the url that will be used for the consent dialog
+ * @param {OAuth2Client} client
+ */
+export function getGoogleAuthUrl(client) {
+    const authorizeUrl = client.generateAuthUrl({
+        access_type: 'offline',
+        scope: ['openid', 'profile', 'email'].join(' '),
+        prompt: 'consent'
+    });
+
+    return authorizeUrl;
+}

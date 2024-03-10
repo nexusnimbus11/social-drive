2afc613d6a2b4e0ad093d75317f2d8555c1a5c83
Prateek Singh <prateeksingh298@gmail.com>
2024-03-10 23:24:36 +0530

    feat: SD-16 configure google oauth2 + temp client page

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,9 @@ import express from 'express';
 import {
     registerUser,
     loginUser,
-    recycleOAuthTokens
+    recycleOAuthTokens,
+    googleAuthCallbackHandler,
+    generateGoogleAuthUrl
 } from '../controllers/authController.js';
 import { verifyOAuthToken } from '../middlewares/auth.middleware.js';
 
@@ -12,5 +14,8 @@ const router = express.Router();
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/tokens/recycle').get(verifyOAuthToken(true), recycleOAuthTokens);
+
+router.route('/oauth/google/generateUrl').get(generateGoogleAuthUrl);
+router.route('/oauth/google').get(googleAuthCallbackHandler);
 
 export default router;
